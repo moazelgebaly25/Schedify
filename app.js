834c14695cfdd2f6e5a88c25a9b030474872c883
moazelgebaly25 <moaz.elgebaly.25@gmail.com>
2026-02-07 17:43:10 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator)
+    try {
+      await navigator.serviceWorker.register("/sw.js");
+    } catch (r) {
+      console.error("Service worker registration failed", r);
+    }
+};
+window.addEventListener("load", () => {
+  registerServiceWorker();
+});
---
+++ sw.js
@@ -0,0 +1,42 @@
+﻿const CACHE_NAME = "schedify-shell-v1",
+  CORE_ASSETS = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/app.js",
+    "/manifest.webmanifest",
+    "/icon.svg",
+  ];
+(self.addEventListener("install", (e) => {
+  (e.waitUntil(caches.open(CACHE_NAME).then((e) => e.addAll(CORE_ASSETS))),
+    self.skipWaiting());
+}),
+  self.addEventListener("activate", (e) => {
+    (e.waitUntil(
+      caches
+        .keys()
+        .then((e) =>
+          Promise.all(
+            e.filter((e) => e !== CACHE_NAME).map((e) => caches.delete(e)),
+          ),
+        ),
+    ),
+      self.clients.claim());
+  }),
+  self.addEventListener("fetch", (e) => {
+    const { request: t } = e;
+    "GET" === t.method &&
+      e.respondWith(
+        caches.match(t).then(
+          (e) =>
+            e ||
+            fetch(t).then((e) => {
+              if (e && 200 === e.status) {
+                const s = e.clone();
+                caches.open(CACHE_NAME).then((e) => e.put(t, s));
+              }
+              return e;
+            }),
+        ),
+      );
+  }));
